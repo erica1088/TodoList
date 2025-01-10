f24a83a8f7cd18c1a681c0cf539cde6b58d3c6ed
ericaacosta <acosta.s.erica@gmail.com>
2025-01-10 12:02:16 -0300

    item

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,40 +1,118 @@
-import React, { useState } from 'react';
-import Item from '../components/Item';
-import { loadTasks } from '../Utils/LocalStorage';
+import { useRef, useState } from "react";
+import {
+  AlertDialog,
+  AlertDialogCloseButton,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Button,
+  IconButton,
+  Text,
+  Box,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { CheckIcon, DeleteIcon } from "@chakra-ui/icons";
 
-const List = () =>{
-  const [tasks , setTasks, saveTasks] = useState(loadTasks())
+import { saveTasks } from "../Utils/LocalStorage";
+import { SiNba } from "react-icons/si";
 
- 
-    const toggleTask = (taskId) => {
-      const updatedTasks = tasks.map((task) =>
-        task.id === taskId ? { ...task, completed: !task.completed } : task
-      );
-      setTasks(updatedTasks);
-      saveTasks(updatedTasks);
-    };
+const Item = ({ task, setTasks, tasks, toggleTask, deleteTask }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = useRef();
+  const [taskIdToDelete, setTaskIdToDelete] = useState(null);
 
-    const deleteTask = (taskId) => {
-      const updatedTasks = tasks.filter((task) => task.id !== taskId);
-      setTasks(updatedTasks);
-      saveTasks(updatedTasks);
-    };
-  
+  // const toggleTask = (taskId) =>  {
+  //   const updatedTasks = tasks.map((task) =>
+  //   task.id === taskId ? {...task, completed: !task.completed} : task)
 
-    return (
-        <div>
-        {tasks.map((task) => (
-          <Item 
-          key={task.id} 
-          task={task} 
-          tasks= {tasks} 
-          toggleTask={toggleTask} 
-          setTasks={setTasks} 
-          deleteTask={deleteTask} />
-        ))}
-      </div>
-     
-    );
+  //   setTasks(updatedTasks);
+  //   saveTasks(updatedTasks);
+  // }
+
+  const handleDeleteTask = () => {
+    const updatedTasks = tasks.filter((task) => task.id !== taskIdToDelete);
+    setTasks(updatedTasks);
+
+    saveTasks(updatedTasks);
+
+    onClose();
+  };
+
+  return (
+    <Box
+      p={{ base: "10px", sm: "15px" }}
+      borderWidth="1px"
+      borderRadius="md"
+      mb="4"
+      display="flex"
+      flexDirection={{ base: "column", sm: "row" }}
+      alignItems="center"
+      justifyContent="space-between"
+    >
+      <Text
+        as={task.completed ? "del" : "span"}
+        flex={1}
+        textDecoration={task.completed ? "line-through" : "none"}
+        fontSize={{ base: "md", sm: "lg" }}
+        color={task.completed ? "blue.800" : "black"}
+      >
+        {task.text}
+      </Text>
+
+      {/* Boton para marcar como completado */}
+      <Box display="flex" gap="10px" mt={{ base: "10px", sm: "0" }}>
+        <IconButton
+          icon={<CheckIcon />}
+          onClick={() => toggleTask(task.id)}
+          colorScheme="teal"
+          aria-label="Marcar como completado"
+          marginRight="10px"
+          size="sm"
+        />
+
+        {/* Boton para elminar tarea */}
+
+        <IconButton
+          icon={<DeleteIcon />}
+          onClick={() => {
+            setTaskIdToDelete(task.id);
+            onOpen();
+          }}
+          colorScheme="red"
+          size="sm"
+          aria-label={`Eliminar tarea: ${task.text}`}
+        />
+      </Box>
+
+      <AlertDialog
+        motionPreset="slideInBottom"
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+        isOpen={isOpen}
+        isCentered
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              ¿Está seguro que desea eliminar esta tarea?
+            </AlertDialogHeader>
+
+            <AlertDialogCloseButton />
+
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                Cancelar
+              </Button>
+              <Button ml={3} colorScheme="red" onClick={handleDeleteTask}>
+                Eliminar
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </Box>
+  );
 };
 
-export default List;
+export default Item;
