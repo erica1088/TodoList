000fd05091bcace1e0c119f5badc1c5f63c5cc70
ericaacosta <acosta.s.erica@gmail.com>
2025-01-10 12:04:47 -0300

    localStorage cambios

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { ChakraProvider } from "@chakra-ui/react";
 import { StrictMode } from 'react'
 import { createRoot } from "react-dom/client";
@@ -14,18 +13,4 @@ createRoot(document.getElementById("root")).render(
     </ChakraProvider>
     
   </StrictMode>
-=======
-import { ChakraProvider,} from '@chakra-ui/react';
-import { createRoot } from 'react-dom/client';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <ChakraProvider>
-    <App />
-  </ChakraProvider>,
-  document.getElementById('root')
->>>>>>> f8464db (modifcaciones)
 );
